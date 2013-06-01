78700107f08674bf566048fe6a75f20dc2a92ef7
Kris Gholson <kris@greenumbrellasoftware.com>
2013-06-01 05:33:41 -0400

    remove unused import

--- src/org/rsta/swimreston/client/Swimreston.java
+++ src/org/rsta/swimreston/client/Swimreston.java
@@ -22,7 +22,6 @@ import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment.HorizontalAlignmentConstant;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.StackLayoutPanel;
